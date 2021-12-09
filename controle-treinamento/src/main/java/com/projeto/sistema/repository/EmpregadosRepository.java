89a6221081585d90e9f3bb029e46eb2360103b86
Gustavo Vieira <60290436+Gustavosantospro@users.noreply.github.com>
2021-12-09 19:44:58 -0300

    Delete controle-treinamento directory

--- controle-treinamento/src/main/java/com/projeto/sistema/repository/EmpregadosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projeto.sistema.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projeto.sistema.model.Empregado;
-
-public interface EmpregadosRepository extends JpaRepository<Empregado, Long> {
-
-}
--- controle-treinamento/src/main/java/com/projeto/sistema/repository/TreinamentosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projeto.sistema.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projeto.sistema.model.Treinamento;
-
-public interface TreinamentosRepository extends JpaRepository<Treinamento, Long> {
-
-}
